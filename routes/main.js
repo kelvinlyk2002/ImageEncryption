188616c0e77e1f23202162855db2686a37e36246
Bubblery <bubblerylimited@protonmail.com>
2022-02-28 18:59:07 -0500

    success pages

--- routes/main.js
+++ routes/main.js
@@ -5,10 +5,18 @@ const upload = multer({ storage: storage });
 
 module.exports = function (app) {
     app.get("/", function (req, res) {
-        res.render("pages/index")
+        res.render("pages/index");
     });
 
     app.post("/", upload.single('image'), function(req, res){
         res.send(req.file);
     });
+
+    app.get("/uploadSuccess",function(req, res){
+        res.render("pages/uploadSuccess");
+    })
+
+    app.get("/decryptSuccess",function(req, res){
+        res.render("pages/decryptSuccess");
+    })
 }
