ecd926d4f8b63d24e8972421e6fccd73b022eef9
Bubblery <bubblerylimited@protonmail.com>
2022-03-10 19:54:55 -0500

    add(testing): added testing for testimage2.jpg testimage3.jpg

--- tests/api/unit.js
+++ tests/api/unit.js
@@ -6,7 +6,6 @@ const fs = require('fs')
 
 chai.use(chaihttp);
 
-
 /*
 
     This file is mainly used to write all the unit tests regarding the application.
@@ -16,11 +15,34 @@ chai.use(chaihttp);
 // This test case makes sure that the encryption is working as expected 
 // By using the encryption mechanism that the service is using.
 describe('Encryption', () => {
-
-    it('the image should not match the encrypted image with key', () => {
+    it('the image should not match the encrypted image with key - testimage1.jpg', () => {
 
         //Arrange
         const data = fs.readFileSync(`${__dirname}/testimage1.jpg`, {encoding: 'base64'})
+
+        //Act
+        const encrypted = encryption.AESCBC('1234567890123456', data, "encrypt")
+
+        //Assert
+        expect(data).to.not.equal(encrypted)
+    });
+
+    it('the image should not match the encrypted image with key - testimage2.jpg', () => {
+
+        //Arrange
+        const data = fs.readFileSync(`${__dirname}/testimage2.jpg`, {encoding: 'base64'})
+
+        //Act
+        const encrypted = encryption.AESCBC('1234567890123456', data, "encrypt")
+
+        //Assert
+        expect(data).to.not.equal(encrypted)
+    });
+
+    it('the image should not match the encrypted image with key - testimage3.jpg', () => {
+
+        //Arrange
+        const data = fs.readFileSync(`${__dirname}/testimage3.jpg`, {encoding: 'base64'})
 
         //Act
         const encrypted = encryption.AESCBC('1234567890123456', data, "encrypt")
@@ -35,10 +57,37 @@ describe('Encryption', () => {
 // By using the encryption mechanism that the service is using.
 describe('Decryption', () => {
 
-    it('the encrypted image should match the image when decrypted with same key', () => {
+    it('the encrypted image should match the image when decrypted with same key - testimage1.jpg', () => {
 
         //Arrange
         const data = fs.readFileSync(`${__dirname}/testimage1.jpg`, {encoding: 'base64'})
+
+        //Act
+        const encrypted = encryption.AESCBC('1234567890123456', data, "encrypt")
+        const decrypted = encryption.AESCBC('1234567890123456', encrypted, "decrypt")
+
+        //Assert
+        expect(data).to.equal(decrypted)
+    });
+
+    it('the encrypted image should match the image when decrypted with same key - testimage2.jpg', () => {
+
+        //Arrange
+        const data = fs.readFileSync(`${__dirname}/testimage2.jpg`, {encoding: 'base64'})
+
+        //Act
+        const encrypted = encryption.AESCBC('1234567890123456', data, "encrypt")
+        const decrypted = encryption.AESCBC('1234567890123456', encrypted, "decrypt")
+
+        //Assert
+        expect(data).to.equal(decrypted)
+    });
+
+
+    it('the encrypted image should match the image when decrypted with same key - testimage3.jpg', () => {
+
+        //Arrange
+        const data = fs.readFileSync(`${__dirname}/testimage3.jpg`, {encoding: 'base64'})
 
         //Act
         const encrypted = encryption.AESCBC('1234567890123456', data, "encrypt")
